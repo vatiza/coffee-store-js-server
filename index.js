9b63489c84e08fc2215964d99746b6b882b284fb
vatiza <rakibur.mac@gmail.com>
2024-04-26 02:04:52 +0600

    data show ui path set

--- index.js
+++ index.js
@@ -26,9 +26,17 @@ async function run() {
     // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
+    const coffeeCollection=client.db('coffeeDB').collection('coffee');
+app.get('/coffee',async (req,res)=>{
+  const cursor=coffeeCollection.find();
+  const result=await cursor.toArray();
+  res.send(result);
+})
     app.post('/coffee',async(req,res)=>{
         const newCoffee=req.body;
         console.log(newCoffee)
+        const result= await coffeeCollection.insertOne(newCoffee);
+        res.send(result);
     })
 
 
