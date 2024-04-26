a36fb1a59296f0a83b71e9961a0f9b658565f515
vatiza <rakibur.mac@gmail.com>
2024-04-26 13:10:43 +0600

    upate coffee

--- index.js
+++ index.js
@@ -31,10 +31,16 @@ async function run() {
 
 app.delete('/coffee/:id',async(req,res)=>{
 const id=req.params.id;
-
 const query= {_id: new ObjectId(id)}
 const result=await coffeeCollection.deleteOne(query)
 res.send(result)
+})
+
+app.get('/coffee/:id',async(req,res)=>{
+  const id=req.params.id;
+  const query={_id: new ObjectId(id)}
+  const result=await coffeeCollection.findOne(query);
+  res.send(result);
 })
 
 
